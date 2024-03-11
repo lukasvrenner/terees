1251aa71a1f2599daba3013b0ee5f6db9c382aca
lukasvrenner <lukasvrenner@icloud.com>
2024-03-10 16:13:18 -0800

    dramaticaly simplified structure of SortedBinaryTree to improve readability, maintainability, and performance

--- src/sorted_binary_tree.rs
+++ src/sorted_binary_tree.rs
@@ -1,83 +1,60 @@
 use crate::Tree;
 
-/// keys on left branches are less than `self.0.key`, while values to the right
-/// are greater than or equal to `self.0.key`
-pub struct SortedBinaryTree<T: PartialOrd>(Option<Node<T>>);
-
-struct Node<T: PartialOrd> {
+/// keys on left branches are less than `self.key`, while values to the right
+/// are greater than or equal to `self.key`
+pub struct SortedBinaryTree<T: PartialOrd> {
     key: T,
-    left: Box<SortedBinaryTree<T>>,
-    right: Box<SortedBinaryTree<T>>,
+    left: Option<Box<SortedBinaryTree<T>>>,
+    right: Option<Box<SortedBinaryTree<T>>>,
 }
 
 impl<T: PartialOrd> SortedBinaryTree<T> {
-    /// returns an optional immutable reference to the left branch of the tree
-    pub fn left(&self) -> Option<&SortedBinaryTree<T>> {
-        Some(self.0.as_ref()?.left.as_ref())
-    }
-
-    /// returns an optional mutable reference to the left branch of the tree
-    pub fn left_mut(&mut self) -> Option<&mut SortedBinaryTree<T>> {
-        Some(self.0.as_mut()?.left.as_mut())
-    }
-
-    /// returns an optional immutable reference to the right branch of the tree
-    pub fn right(&self) -> Option<&SortedBinaryTree<T>> {
-        Some(self.0.as_ref()?.right.as_ref())
-    }
-
-    /// returns an optional mutable reference to the right branch of the tree
-    pub fn right_mut(&mut self) -> Option<&mut SortedBinaryTree<T>> {
-        Some(self.0.as_mut()?.right.as_mut())
-    }
-}
-
-impl<T: PartialOrd> SortedBinaryTree<T> {
-    fn new() -> SortedBinaryTree<T> {
-        SortedBinaryTree(None)
+    fn new(key: T) -> SortedBinaryTree<T> {
+        SortedBinaryTree {
+            key,
+            left: None,
+            right: None,
+        }
     }
 }
 
 impl<T: PartialOrd> Tree for SortedBinaryTree<T> {
     type Item = T;
-    
-    /// adds a new node with a key `key` to the tree
+
     fn add(&mut self, key: Self::Item) {
         // check left branch
-        if self.0.as_mut().is_some_and(|node| key < node.key) {
-            match self.left_mut() {
-                Some(tree) => return tree.add(key),
+        if self.key < key {
+            match &mut self.left {
+                Some(ref mut tree) => return tree.add(key),
                 None => {
-                    self.0.as_mut().unwrap().left =
-                        Box::new(SortedBinaryTree(Some(Node {
-                            key,
-                            left: Box::new(SortedBinaryTree(None)),
-                            right: Box::new(SortedBinaryTree(None)),
-                        })));
-                    return;
-                }
-            }
-        } 
-        // check right branch
-        if self.0.as_mut().is_some_and(|node| key >= node.key) {
-            match self.right_mut() {
-                Some(tree) => return tree.add(key),
-                None => {
-                    self.0.as_mut().unwrap().right =
-                        Box::new(SortedBinaryTree(Some(Node {
-                            key,
-                            left: Box::new(SortedBinaryTree(None)),
-                            right: Box::new(SortedBinaryTree(None)),
-                        })));
+                    self.left = Some(Box::new(SortedBinaryTree {
+                        key,
+                        left: None,
+                        right: None,
+                    }));
                     return;
                 }
             }
         }
-        *self = SortedBinaryTree(Some(Node {
+        // check right branch
+        if self.key >= key {
+            match &mut self.right {
+                Some(ref mut tree) => return tree.add(key),
+                None => {
+                    self.right = Some(Box::new(SortedBinaryTree {
+                        key,
+                        left: None,
+                        right: None,
+                    }));
+                    return;
+                }
+            }
+        }
+        *self = SortedBinaryTree {
             key,
-            left: Box::new(SortedBinaryTree(None)),
-            right: Box::new(SortedBinaryTree(None)),
-        }))
+            left: None,
+            right: None,
+        };
     }
 
     fn remove(&mut self, key: Self::Item) {
@@ -85,27 +62,21 @@ impl<T: PartialOrd> Tree for SortedBinaryTree<T> {
     }
 
     fn find(&self, key: Self::Item) -> Option<&Self> {
-        if self.0.as_ref().is_some_and(|node| key < node.key) {
-            return self.left()?.find(key);
+        if self.key < key {
+            return self.left.as_deref()?.find(key);
         }
-        if self.0.as_ref().is_some_and(|node| key > node.key) {
-            return self.right()?.find(key);
-        }
-        if self.0.is_none() {
-            return None;
+        if self.key > key {
+            return self.right.as_deref()?.find(key);
         }
         Some(self)
     }
 
     fn find_mut(&mut self, key: Self::Item) -> Option<&mut Self> {
-        if self.0.as_mut().is_some_and(|node| key < node.key) {
-            return self.left_mut()?.find_mut(key);
+        if self.key < key {
+            return self.left.as_deref_mut()?.find_mut(key);
         }
-        if self.0.as_mut().is_some_and(|node| key > node.key) {
-            return self.right_mut()?.find_mut(key);
-        }
-        if self.0.is_none() {
-            return None;
+        if self.key > key {
+            return self.right.as_deref_mut()?.find_mut(key);
         }
         Some(self)
     }
